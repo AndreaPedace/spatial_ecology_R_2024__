24a6c8c70b2f7f20ff3d81ec4573e3d40060d3ed
AndreaPedace <andrea.pedace92@gmail.com>
2024-10-09 15:54:42 +0200

    Create my_first_R_script.R

---
+++ Code/my_first_R_script.R
@@ -0,0 +1,37 @@
+#R can be used as a calculator 
+2+3
+
+sonia <- 2+3 #I  assign the result of the operation to an object
+sonia
+
+lea <-3+4
+lea
+
+sonia+lea
+sonia^lea
+
+c(10,20,30,50,70) #this is an array 
+ tommaso <- c(10,20,30,50,70)
+ tommaso
+[1] 10 20 30 50 70
+erica <- c(1,3,5,7,9)
+erica
+
+#creare una relazione
+plot(tommaso,erica)
+
+#changing point character or color
+#point character
+plot(tommaso, Erica, pch=18)
+
+#colors
+plot(tommaso, Erica, col="blue")
+
+#character exaggeration
+> plot(tommaso, Erica, col="blue", pch=18, cex=2)
+
+#change labels of the table
+> plot(tommaso, Erica, col="blue", pch=18, cex=2, xlab="cannbis", ylab="dolphin")
+
+
+
