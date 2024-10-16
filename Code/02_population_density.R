4a05e02f7220260cc04cced0da63deaf6f96e03f
AndreaPedace <andrea.pedace92@gmail.com>
2024-10-16 15:33:39 +0200

    Update 02_population_density.R

--- Code/02_population_density.R
+++ Code/02_population_density.R
@@ -34,3 +34,48 @@ plot (bei)
 #connecting two different type of data
 plot(densitymap)
 > points(bei, col="green", cex=.5)
+
+#DAY2
+
+#recall the library/package
+library(spatstat)
+
+bei
+
+densitymap <- density(bei)
+points(bei)
+
+bei.extra
+elevation <-bei.extra$elev
+elevation2 <-bei.extra[[1]]
+
+#possibility to  build  multiframe image
+par(mfrow=c(1,2))
+plot(elevation2)
+plot(densitymap)
+
+#multiframe one on top of the  other
+par(mfrow=c(2,1))
+plot(elevation2)
+plot(densitymap)
+
+#how to delete multiframe
+dev.off()
+
+#changing color  of the map
+colorRampPalette(c("red", "orange", "yellow"))(3)
+cl <- colorRampPalette(c("red", "orange", "yellow"))(3)
+plot(densitymap, col=cl)
+
+#more gradients
+colorRampPalette(c("red", "orange", "yellow"))(10)
+
+#excercise: build a multiframe and plot two different densitymaps with two different palettes
+cl <-colorRampPalette(c("olivedrab1", "paleturquoise", "grey", "orangered"))(100)
+plot(densitymap, col=cl)
+cl2 <-colorRampPalette(c("red", "orange", "yellow"))(100)
+par(mfrow=c(1,2))
+plot(densitymap, col=cl)
+plot(densitymap, col=cl2)
+ 
+
