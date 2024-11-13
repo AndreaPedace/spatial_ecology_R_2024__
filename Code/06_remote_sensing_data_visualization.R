c6fe09b459a0a3a441e126b92726e25fe889b37f
Andrea Pedace <andrea.pedace92@gmail.com>
2024-11-13 15:43:30 +0100

    Create 06_remote_sensing_data_visualization.R

---
+++ Code/06_remote_sensing_data_visualization.R
@@ -0,0 +1,24 @@
+#code for mananging and analyzing remote sensing data 
+
+#there are same packages that are in Github and not in Cran R
+install_github("")
+
+#but first we need to install a package called "devtools"
+install.packages("devtools")
+
+library("devtools")
+
+install_github("ducciorocchini/imageRy")
+library(imageRy)
+
+#install the packages for Mac
+install.packages("/Users/lea/Desktop/R/ducciorocchini-imageRy-a971c18.tar.gz",repos = NULL, type = "source")
+install.packages("dichromat")  
+install.packages("fields") 
+install.packages("ggplot2") 
+install.packages("viridis") 
+
+library(imageRy)
+
+
+  
