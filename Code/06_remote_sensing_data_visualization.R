f3a22a0cab8268ec488b58d6297919c718879afc
Andrea Pedace <andrea.pedace92@gmail.com>
2024-11-20 14:53:31 +0100

    Update 06_remote_sensing_data_visualization.R

--- Code/06_remote_sensing_data_visualization.R
+++ Code/06_remote_sensing_data_visualization.R
@@ -11,7 +11,7 @@ library("devtools")
 install_github("ducciorocchini/imageRy")
 library(imageRy)
 
-#install the packages for Mac
+#install the packages for Mac, don't forget to download the file from the link
 https://api.github.com/repos/ducciorocchini/imageRy/tarball/HEAD
   
 install.packages("/Users/lea/Desktop/R/ducciorocchini-imageRy-a971c18.tar.gz",repos = NULL, type = "source")
@@ -21,6 +21,36 @@ install.packages("ggplot2")
 install.packages("viridis") 
 
 library(imageRy)
+library(terra)
 
+#see all the data on the package
+im.list()
 
+#import data (any function on GitHub start with "Im.")
+b2<- im.import("sentinel.dolomites.b2.tif")
   
+#change color palette
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
+
+#let's import another band and change the palette
+b3<- im.import("sentinel.dolomites.b3.tif")
+plot(b3, color=cl)
+
+
+#let's import another band and change the palette
+b4<- im.import("sentinel.dolomites.b4.tif")
+plot(b4, col=cl)
+
+#let's import another band and change the palette
+b8<- im.import("sentinel.dolomites.b8.tif")
+
+#let's plot all together (multiframe)
+par(mfrow=c(2,2))
+plot(b8, col=cl)
+plot(b4, col=cl)
+plot(b3, col=cl)
+plot(b2, col=cl)
+
+#stack (unite the different images)
+sentstack<-(b2,b3,b4,b8)
+plot(sentstack, col=cl)
