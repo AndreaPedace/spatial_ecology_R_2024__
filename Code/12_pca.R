9b697d48394d4d4ed4022868b68aeb79ee72fe4f
Andrea Pedace <andrea.pedace92@gmail.com>
2024-12-12 15:45:24 +0100

    Update 12_pca.R

--- Code/12_pca.R
+++ Code/12_pca.R
@@ -1,1 +1,18 @@
+library(imageRy)
+library(terra)
+library(viridis)
 
+im.list()
+
+sent <- im.import("sentinel.png")
+
+pairs(sent)
+
+sentpc<-im.pca(sent)
+pc1<-sentpc$PC1
+plot(sentpc)
+
+pc1<-sentpc[[1]]
+
+pc1sd<-focal(pc1, matrix(1/9,3,3),fun=sd)
+plot(pc1sd)
